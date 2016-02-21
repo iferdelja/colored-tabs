8a561e741265083bc3536b825fc62dda386b6ac3
Ivan Ferdelja <ivan.ferdelja@gmail.com>
2016-02-21 22:16:30 +0100

    Style update.

--- android/ColoredTabs/app/src/main/java/com/ivanferdelja/coloredtabs/MainActivity.java
+++ android/ColoredTabs/app/src/main/java/com/ivanferdelja/coloredtabs/MainActivity.java
@@ -8,12 +8,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AbsListView;
-import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
